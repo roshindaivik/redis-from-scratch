9b00b425cb3101e3179cc7af3de8d40f2530c535
roshindaivik <roshin@thealteroffice.com>
2025-12-13 12:31:17 +0530

    list operations added

--- handler/conn_handler.go
+++ handler/conn_handler.go
@@ -18,14 +18,15 @@ const (
 )
 
 type Value struct{
-	Data interface{}
-	Expiry *time.Time 
-	Type ValueType	
+	data interface{}
+	ttl *time.Time 
+	typ ValueType	
 }
 
 type DB struct {
-	mu sync.Mutex
+	mu sync.RWMutex
 	kv map[string]Value
+	conds map[string]*sync.Cond
 }
 
 type Command struct {
@@ -35,52 +36,88 @@ type Command struct {
 
 var db = DB{
 	kv: make(map[string]Value),
+	conds: make(map[string]*sync.Cond),
 }
 
 
 func RedisConnHandler(conn net.Conn) {
 	defer conn.Close()
-	fmt.Println("Client connected:", conn.RemoteAddr())
+	writeRESP(conn, "Client connected: %v\r\n", conn.RemoteAddr())
 
 	reader := bufio.NewReader(conn)
 
 	for {
 		msg, err := reader.ReadString('\n')
 		if err != nil {
-			fmt.Println("Client disconnected:", err)
+			writeRESP(conn, "Client disconnected: %v\r\n", err)
 			return
 		}
 
 		command := ParseCommands(msg)
 		if command.cmd == "" {
-			conn.Write([]byte("-ERR empty command\r\n"))
+			writeRESP(conn, "-ERR empty command\r\n")
 			continue
 		}
 
 		switch command.cmd {
 		case "PING":
-			conn.Write([]byte("+PONG\r\n"))
+			writeRESP(conn, "+PONG\r\n")
 		case "SET":
 			if err := db.SetKey(command.args); err != nil {
-				conn.Write([]byte(fmt.Sprintf("-ERR %s\r\n", err.Error())))
+				writeRESP(conn, "-ERR %s\r\n", err.Error())
 			} else {
-				conn.Write([]byte("+OK\r\n"))
+				writeRESP(conn, "+OK\r\n")
 			}
 		case "GET":
 			val, ok := db.GetKey(command.args)
 			if !ok {
-				conn.Write([]byte("nil\r\n")) // Redis style nil
+				writeRESP(conn, "nil\r\n")
 			} else {
-				conn.Write([]byte(val+"\r\n"))
+				writeRESP(conn, "%s\r\n", val)
 			}
 		case "ECHO":
 			if len(command.args) == 0 {
-				conn.Write([]byte("-ERR wrong number of arguments for 'ECHO'\r\n"))
+				writeRESP(conn, "-ERR wrong number of arguments for 'ECHO'\r\n")
 			} else {
-				conn.Write([]byte(fmt.Sprintf("+%s\r\n", strings.Join(command.args, " "))))
+				writeRESP(conn, "+%s\r\n", strings.Join(command.args, " "))
+			}
+		case "RPUSH":
+			err := db.RPush(command.args)
+			if err != nil{
+				writeRESP(conn, "-ERR %s\r\n", err.Error())
+			}else{
+				writeRESP(conn, "+OK\r\n")
+			}
+		case "LRANGE":
+			data, err := db.LRange(command.args)
+			if err != nil{
+				writeRESP(conn, "-ERR %s\r\n", err.Error())
+			}else{
+				writeRESP(conn, "%s\r\n", data)
+			}
+		case "LLEN":
+			data,err := db.LLen(command.args)
+			if err != nil{
+				writeRESP(conn, "-ERR %s\r\n", err.Error())
+			}else{
+				writeRESP(conn, "%v\r\n", data)
+			}
+		case "LPOP":
+			data,err := db.LPop(command.args)
+			if err != nil{
+				writeRESP(conn, "-ERR %s\r\n", err.Error())
+			}else{
+				writeRESP(conn, "%s",data)
+			}
+		case "BLOP":
+			data,err := db.Blop(command.args)
+			if err != nil{
+				writeRESP(conn, "-ERR %s\r\n", err.Error())
+			}else{
+				writeRESP(conn, "%s\r\n",data)
 			}
 		default:
-			conn.Write([]byte(fmt.Sprintf("-ERR unknown command '%s'\r\n", command.cmd)))
+			writeRESP(conn, "-ERR unknown command '%s'\r\n", command.cmd)
 		}
 	}
 }
@@ -89,14 +126,17 @@ func (db *DB) GetKey(args []string) (string, bool) {
 	if len(args) < 1 {
 		return "", false
 	}
-	db.mu.Lock()
-	defer db.mu.Unlock()
+	db.mu.RLock()
+	defer db.mu.RUnlock()
 	val, ok := db.kv[args[0]]
-	if val.Expiry != nil && time.Now().After(*val.Expiry){
-		delete(db.kv,args[0])
-		return "",false
+	if !ok {
+		return "", false
 	}
-	return val.Data.(string), ok
+	if val.ttl != nil && time.Now().After(*val.ttl){
+		delete(db.kv, args[0])
+		return "", false
+	}
+	return val.data.(string), ok
 }
 
 func (db *DB) SetKey(args []string) error {
@@ -104,31 +144,231 @@ func (db *DB) SetKey(args []string) error {
 		return fmt.Errorf("wrong number of arguments for 'SET'")
 	}
 	key := args[0]
+	var ttl *time.Time
 	value := args[1]
-
-	var expiry *time.Time;
-
 	if len(args) > 2 && args[2] == "PX"{
 		if len(args) < 4{
 			return fmt.Errorf("syntax error")
 		}
-		// Somehow set a timer for it to expire 
 		ms, err := strconv.ParseInt(args[3], 10, 64)
 		if err != nil{
 			return fmt.Errorf("invalid expire time in 'SET'")
 		}
-		expiryTime := time.Now().Add(time.Duration(ms) * time.Second)
-		expiry = &expiryTime	
-	}
+		expiryTime := time.Now().Add(time.Duration(ms) * time.Millisecond)
+		ttl = &expiryTime	
+	}	
 	db.mu.Lock()
 	defer db.mu.Unlock()
 	db.kv[key] = Value{
-		Data: value,
-		Expiry: expiry,	
-		Type: STRING,
+		data: value,
+		ttl: ttl,	
+		typ: STRING,
 	}
-	fmt.Println(db)
 	return nil
+}
+
+
+// NOT HANDLING LPUSH FOR NOW 
+func (db *DB) RPush(args []string) error{
+	if len(args) < 2 {
+		return fmt.Errorf("wrong number of arguments for 'RPUSH'")
+	}
+	key := args[0]
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	val, ok := db.kv[key]
+	if ok{
+		if val.typ != LIST{
+			return fmt.Errorf("WRONGTYPE Operation against a key holding the wrong kind of value")
+		}
+		for _, arg := range args[1:]{			
+			val.data = append(val.data.([]interface{}), arg)
+		}
+		db.kv[key] = val
+	}else{
+		data := make([]interface{}, 0, len(args)-1)
+		for _, arg := range args[1:]{
+			data = append(data, arg)
+		}
+		db.kv[key] = Value{
+			data: data,
+			typ: LIST,
+		}
+	}
+	if cond,ok := db.conds[key];ok{
+		cond.Broadcast()
+	}
+	return nil 
+}
+
+func (db *DB) Blop(args []string)(string,error){
+	if len(args) < 2{
+		return "",fmt.Errorf("wrong number of arguments for 'BLOP'")
+	}
+	key := args[0]
+	timer, err := strconv.Atoi(args[1])
+    if err != nil || timer < 0 {
+        return "", fmt.Errorf("value is not an integer or out of range")
+    }
+    
+    var deadline time.Time
+    hasDeadline := timer > 0
+    if hasDeadline {
+        deadline = time.Now().Add(time.Duration(timer) * time.Second)
+    }
+    db.mu.Lock()
+	defer db.mu.Unlock()
+	if _, ok := db.conds[key]; !ok {
+		db.conds[key] = sync.NewCond(&db.mu)
+	}
+	cond := db.conds[key]
+	if hasDeadline{
+		stopTimer := make(chan bool)
+		defer close(stopTimer)
+		go func(){
+			ticker := time.NewTicker(1 * time.Second)
+			defer ticker.Stop()
+			for{
+				select{
+				case <-ticker.C:
+					db.mu.Lock()
+					cond.Broadcast()
+					db.mu.Unlock()
+				case <-stopTimer:
+					return 
+				}
+			}
+		}()
+	}
+	for {
+		val,ok := db.kv[key]
+		if ok{
+			if val.typ != LIST{
+				return "",fmt.Errorf("WRONGTYPE Operation against a key holding the wrong kind of value")
+			}
+			data := val.data.([]interface{})
+			if len(data) > 0 {
+				removedData := data[:1]
+				val.data = data[1:]
+				db.kv[key] = val
+				return formRESPArray(removedData),nil
+			}
+		}
+		if hasDeadline && time.Now().After(deadline){
+			return "",fmt.Errorf("timer expired")
+		}
+		cond.Wait()
+	}	
+
+}
+
+func (db * DB) LLen(args []string)(int,error){
+	if len(args) < 1{
+		return 0,fmt.Errorf("wrong number of arguments for 'LLEN'")
+	}
+	key := args[0]
+	db.mu.RLock()
+	defer db.mu.RUnlock()
+	val,ok := db.kv[key]
+	if !ok{
+		return 0,nil
+	}
+	if val.typ != LIST{
+		return 0, fmt.Errorf("WRONGTYPE Operation against a key holding the wrong kind of value")
+	}
+	data := val.data.([]interface{})
+	return len(data),nil
+}
+
+func (db *DB) LPop(args []string)(string,error){
+	argLen := len(args)
+	if argLen < 1 || argLen >= 3{
+		return "",fmt.Errorf("wrong number of arguments for 'LLEN'")
+	}
+	key := args[0]
+	num := 1 
+	if argLen == 2{
+		ele, err := strconv.Atoi(args[1])
+		if err != nil {
+			return "", fmt.Errorf("value is not an integer or out of range")
+		}
+		num = ele
+	}
+	db.mu.Lock()
+	defer db.mu.Unlock()
+	val, ok := db.kv[key]
+	if !ok {
+		return "", nil
+	}
+	if val.typ != LIST {
+		return "", fmt.Errorf("WRONGTYPE Operation against a key holding the wrong kind of value")
+	}
+	data := val.data.([]interface{})
+	n := len(data)
+	if num < 0{
+		num = n + num
+	}
+	if num > n{
+		num = n-1
+		removedData := data[:num]
+		delete(db.kv, key)
+		return formRESPArray(removedData),nil
+	}else{
+		removedData := data[:num]
+		val.data = data[num:]
+		db.kv[key] = val
+		return formRESPArray(removedData),nil
+	}
+}
+
+
+func (db *DB) LRange(args []string)(string, error){
+	if len(args) < 3{
+		return "", fmt.Errorf("wrong number of arguments for 'LRANGE'")
+	}
+	key := args[0]
+	start, err := strconv.Atoi(args[1])
+	if err != nil {
+		return "", fmt.Errorf("value is not an integer or out of range")
+	}
+	end, err := strconv.Atoi(args[2])
+	if err != nil {
+		return "", fmt.Errorf("value is not an integer or out of range")
+	}
+	
+	db.mu.RLock()
+	defer db.mu.RUnlock()	
+	val, ok := db.kv[key]
+	if !ok {
+		return "", nil
+	}
+	if val.typ != LIST {
+		return "", fmt.Errorf("WRONGTYPE Operation against a key holding the wrong kind of value")
+	}
+	
+	data := val.data.([]interface{})
+	n := len(data)
+	
+	if start < 0{
+		start += n 
+	}
+	if end < 0 {
+		end += n
+	}
+	if start < 0 {
+		start = 0
+	}
+	if start >= n {
+		return "", nil
+	}
+	if end >= n{
+		end = n - 1
+	}
+	if start > end {
+		return "", nil
+	}
+	
+	return formRESPArray(data[start:end+1]), nil
 }
 
 func ParseCommands(msg string) Command {
@@ -145,4 +385,22 @@ func ParseCommands(msg string) Command {
 		cmd:  cmd,
 		args: args,
 	}
+}
+
+func formRESPArray(data []any) string{
+	var buf strings.Builder
+	buf.WriteString(fmt.Sprintf("Length %d\r\n", len(data)))
+	for _, s := range data{
+		buf.WriteString(fmt.Sprintf("%v\r\n", s))
+	}
+	return buf.String()
+}
+
+func writeRESP(conn net.Conn, s string, args ...any){
+	if len(args) > 0 {
+		fmt.Fprintf(conn, s, args...)
+	} else {
+		fmt.Fprint(conn, s)
+	}
+	fmt.Fprint(conn, "> ")
 }
